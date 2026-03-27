e8500817765ceca5bb4107c1b2f6a5838070e582
Brandon Trautner <164392471+brandonTraut@users.noreply.github.com>
2026-03-27 13:43:37 -0600

    Add delivery performance SQL query

---
+++ sql/03_delivery_performance.sql
@@ -0,0 +1,12 @@
+SELECT order_id
+	,order_date
+	,delivered_date
+	,estimated_delivery_date
+	,CASE 
+		WHEN delivered_date <= estimated_delivery_date
+			THEN 'On Time'::TEXT
+		ELSE 'Late'::TEXT
+		END AS delivery_status
+	,EXTRACT(day FROM delivered_date - order_date) AS actual_days
+	,EXTRACT(day FROM estimated_delivery_date - order_date) AS estimated_days
+FROM v_orders_complete;
