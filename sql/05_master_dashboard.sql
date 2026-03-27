146928884114e615d5d5234272557efb59eaaa89
Brandon Trautner <164392471+brandonTraut@users.noreply.github.com>
2026-03-27 13:46:56 -0600

    Add SQL query for master dashboard

---
+++ sql/05_master_dashboard.sql
@@ -0,0 +1,20 @@
+SELECT order_id
+	,customer_id
+	,customer_state
+	,customer_city
+	,order_date
+	,delivered_date
+	,estimated_delivery_date
+	,order_revenue
+	,freight_value
+	,item_count
+	,payment_types
+	,total_payment_value
+	,review_score
+	,date_trunc('month'::TEXT, order_date) AS order_month
+	,CASE 
+		WHEN delivered_date <= estimated_delivery_date
+			THEN 'On Time'::TEXT
+		ELSE 'Late'::TEXT
+		END AS delivery_status
+FROM v_orders_complete o;
